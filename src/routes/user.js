b0c7b4c021455d4766da09b099fea29a326e4b54
njs04210 <pearlnkim@gmail.com>
2021-04-06 16:01:30 +0900

    change code

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,5 +4,7 @@ const UserController = require('../controller/UserController');
 
 //Login
 router.post('/', UserController.login);
+//회원 자녀나이, 지역, 닉네임 등록
+router.patch('/', UserController.setInfo);
 
 module.exports = router;
