63717bd2db07147b7f1f9580f6eac46687e14c06
KimJinJoo <pearlnkim@gmail.com>
2021-06-02 10:45:35 +0900

    Feature/API-AnalysisFragment (#27)
    * refactoring
    
    * modify code for analysis

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
 const jwt = require('../modules/jwt');
 const userModel = require('../models/user');
+const bookreportModel = require('../models/bookreport');
+const bookshelfModel = require('../models/bookshelf');
 const serviceAccount = require('../config/serviceAccountKey.json');
 const statusCode = require('../modules/statusCode');
 
@@ -59,16 +61,85 @@ exports.setInfo = async (req, res) => {
 
 exports.getInfo = async (req, res) => {
   const memId = req.params.memId;
-  const memInfo = await userModel.find(memId);
+  var memInfo = await userModel.find(memId);
   const kidsInfo = await userModel.getKids(memId);
   if (memInfo == undefined) {
     res.status(statusCode.NOT_FOUND).json({ code: 0 });
   } else {
-    res.status(statusCode.OK).json({
-      code: 21,
-      msg: '회원 정보 불러오기 성공',
-      user: memInfo,
-      kids: { sex: kidsInfo.sex, age: kidsInfo.age },
-    });
+    var flag = delete memInfo['password'];
+    if (flag) {
+      res.status(statusCode.OK).json({
+        code: 21,
+        msg: '회원 정보 불러오기 성공',
+        user: memInfo,
+        kids: { sex: kidsInfo.sex, age: kidsInfo.age },
+      });
+    }
+  }
+};
+
+exports.getAnalysis = async (req, res) => {
+  const memId = req.memId;
+  var result = {};
+  var kidsData = await userModel.getKids(memId);
+  var bookshelfData = await bookshelfModel.find(memId);
+  if (bookshelfData != undefined) {
+    bookshelfData = JSON.parse(JSON.stringify(bookshelfData));
+    var bookshelf_id = bookshelfData.bookshelf_id;
+    var countCategoryData = await bookshelfModel.countBooksCategory(
+      bookshelf_id
+    ); // 카테고리 별로 몇개?
+    if (countCategoryData != 0) {
+      var totalBooks = await bookshelfModel.getTotalBooks(bookshelf_id); // 총 책 개수
+
+      var NoReportData = await bookreportModel.findItemNoReport(bookshelf_id);
+      var totalNoReport;
+      if (NoReportData == 0) {
+        totalNoReport = 0;
+      } else {
+        totalNoReport = NoReportData.length;
+      }
+      var totalYesReport = totalBooks - totalNoReport; // 독후감 총 개수
+      var countingRead = await bookshelfModel.countBooksDate(bookshelf_id); // 오늘, 지난1주일, 지난 2주일 읽은 책 수
+
+      result.countCategory = countCategoryData;
+      result.totalBooks = totalBooks;
+      result.totalReports = {
+        Not_Written: totalNoReport,
+        Written: totalYesReport,
+      };
+      result.totalPerDate = countingRead;
+
+      var kidsId = await bookshelfModel.getBookshelfId_sameKids(
+        memId,
+        kidsData.age
+      );
+      if (kidsId != 0) {
+        var totalOthers = 0;
+        for (let i = 0; i < kidsId.length; i++) {
+          var oneData = await bookshelfModel.countBooksDate(kidsId[i]);
+          totalOthers += oneData.minus;
+          if (i == kidsId.length - 1) {
+            var avg = totalOthers / kidsId.length; //  내 나이, 성별과 같은 아이는 저번주보다 이번주에 얼마만큼 더 읽었는지 (평균)
+            result.avg_Others = avg;
+            res
+              .status(statusCode.OK)
+              .json({ code: 82, msg: '책장 분석 성공', result: result });
+          }
+        }
+      } else {
+        res
+          .status(statusCode.OK)
+          .json({ code: 81, msg: '책장 분석 성공', result: result });
+      }
+    } else {
+      res
+        .status(statusCode.NOT_FOUND)
+        .json({ code: 84, msg: '등록된 책이 없음' });
+    }
+  } else {
+    res
+      .status(statusCode.NOT_FOUND)
+      .json({ code: 83, msg: '책장이 존재하지 않음' });
   }
 };
--- src/models/bookshelf.js
+++ src/models/bookshelf.js
@@ -1,19 +1,31 @@
 const db = require('../config/db');
 const Promise = require('es6-promise').Promise;
 
-exports.findAll = function (memId) {
-  return new Promise((resolve, reject) => {
-    db.query(
-      `SELECT * FROM Bookshelf WHERE mem_id != ?`,
-      [memId],
-      function (err, res) {
-        if (err) reject(err);
-        else {
-          const bookshelf = JSON.parse(JSON.stringify(res));
-          resolve(bookshelf);
+exports.findAll = function (memId, query_age) {
+  var qs_age = '';
+  var sql = `SELECT * FROM Bookshelf AS shelf JOIN Kids AS kids ON shelf.mem_id = kids.mem_id WHERE shelf.mem_id != ?`;
+  if (query_age != undefined) {
+    for (let i = 0; i < query_age.length; i++) {
+      if (query_age.length == 1) {
+        qs_age += ` AND kids.age = ` + query_age[0];
+      } else {
+        if (i == 0) {
+          qs_age += ` AND kids.age >= ` + query_age[i];
+        } else if (i != 0) {
+          qs_age += ` AND kids.age <= ` + query_age[i];
         }
       }
-    );
+    }
+    sql += qs_age;
+  }
+  return new Promise((resolve, reject) => {
+    db.query(sql, [memId], function (err, res) {
+      if (err) reject(err);
+      else {
+        const bookshelf = JSON.parse(JSON.stringify(res));
+        resolve(bookshelf);
+      }
+    });
   });
 };
 
@@ -45,57 +57,68 @@ exports.findItem = function (bookshelf_id, book_id) {
   });
 };
 
-exports.getBooks = function (bookshelf_id) {
+exports.getBooks = function (bookshelf_id, query_category) {
+  var qs_category = '';
+  var sql = `SELECT book_id from Bookshelf_item WHERE bookshelf_id = ?`;
+  if (query_category != undefined) {
+    for (let i = 0; i < query_category.length; i++) {
+      if (i == 0) {
+        qs_category += ` AND (category = ` + query_category[i];
+      } else if (i != 0) {
+        qs_category += ` OR category = ` + query_category[i];
+      }
+      if (i == query_category.length - 1) {
+        qs_category += ` )`;
+      }
+    }
+    sql += qs_category;
+  }
   return new Promise((resolve, reject) => {
-    db.query(
-      `SELECT book_id from Bookshelf_item WHERE bookshelf_id = ?`,
-      [bookshelf_id],
-      function (err, res) {
-        if (err) reject(err);
-        else if (res[0] == undefined) {
-          //책장에 책이 없는 경우
-          resolve(res);
-        } else {
-          var booklist = new Array();
-          const books_id = JSON.parse(JSON.stringify(res));
-          books_id.forEach(function (element) {
-            booklist.push(element['book_id']);
-          });
+    db.query(sql, [bookshelf_id], function (err, res) {
+      if (err) reject(err);
+      else if (res[0] == undefined) {
+        //책장에 책이 없는 경우
+        resolve(res);
+      } else {
+        var booklist = new Array();
+        const books_id = JSON.parse(JSON.stringify(res));
+        books_id.forEach(function (element) {
+          booklist.push(element['book_id']);
+        });
 
-          var JsonArray = new Array();
-          for (let i = 0; i < booklist.length; i++) {
-            var book_id = booklist[i];
-            db.query(
-              `SELECT * FROM Book WHERE book_id = ?`,
-              [book_id],
-              function (err, res) {
-                if (err) reject(err);
-                else {
-                  var book_info = JSON.parse(JSON.stringify(res[0]));
-                  db.query(
-                    `SELECT count(book_id) AS total FROM Bookshelf_item WHERE book_id = ?`,
-                    [book_info.book_id],
-                    function (err, res) {
-                      if (err) reject(err);
-                      else {
-                        var total = parseInt(
-                          JSON.parse(JSON.stringify(res[0].total))
-                        );
-                        book_info.total_inShelf = total;
-                        JsonArray.push(book_info);
-                        if (i == booklist.length - 1) {
-                          resolve(JsonArray);
-                        }
+        var JsonArray = new Array();
+        for (let i = 0; i < booklist.length; i++) {
+          var book_id = booklist[i];
+          db.query(
+            `SELECT * FROM Book WHERE book_id = ?`,
+            [book_id],
+            function (err, res) {
+              if (err) reject(err);
+              else {
+                var book_info = JSON.parse(JSON.stringify(res[0]));
+                db.query(
+                  `SELECT count(book_id) AS total FROM Bookshelf_item WHERE book_id = ?`,
+                  [book_info.book_id],
+                  function (err, res) {
+                    if (err) reject(err);
+                    else {
+                      var total = parseInt(
+                        JSON.parse(JSON.stringify(res[0].total))
+                      );
+                      book_info.total_inShelf = total;
+                      JsonArray.push(book_info);
+                      if (i == booklist.length - 1) {
+                        resolve(JsonArray);
                       }
                     }
-                  );
-                }
+                  }
+                );
               }
-            );
-          }
+            }
+          );
         }
       }
-    );
+    });
   });
 };
 
@@ -195,7 +218,7 @@ exports.addBook = function (memId, book) {
                             else {
                               const book_id = res[0].book_id;
                               db.query(
-                                `INSERT INTO Bookshelf_item (bookshelf_id, book_id, category) VALUES (?,?,?)`,
+                                `INSERT INTO Bookshelf_item (bookshelf_id, book_id, category, created) VALUES (?,?,?, NOW())`,
                                 [bookshelf_id, book_id, book.category],
                                 function (err) {
                                   if (err) reject(err);
@@ -227,7 +250,7 @@ exports.addBook = function (memId, book) {
                       else {
                         if (res[0] == undefined) {
                           db.query(
-                            `INSERT INTO Bookshelf_item (bookshelf_id, book_id, category) VALUES (?,?, ?)`,
+                            `INSERT INTO Bookshelf_item (bookshelf_id, book_id, category, created) VALUES (?,?,?,NOW())`,
                             [bookshelf_id, book_id, book.category],
                             function (err) {
                               if (err) reject(err);
@@ -259,25 +282,6 @@ exports.addBook = function (memId, book) {
   });
 };
 
-/* exports.getReportsCounting = function () {
-  return new Promise((resolve, reject) => {
-    db.query(
-      `SELECT bookshelf_id, count(bookreport_id) AS total FROM Bookshelf_item WHERE bookreport_id IS NOT NULL GROUP BY bookshelf_id ORDER BY total DESC limit 3`,
-      function (err, res) {
-        if (err) reject(err);
-        else {
-          var result = new Array();
-          for (let i = 0; i < res.length; i++) {
-            var dataSet = JSON.parse(JSON.stringify(res[i]));
-            result.push(dataSet);
-          }
-          resolve(result);
-        }
-      }
-    );
-  });
-}; */
-
 exports.getReportsCounting = function () {
   return new Promise((resolve, reject) => {
     db.query(
@@ -291,6 +295,124 @@ exports.getReportsCounting = function () {
             result.push(dataSet);
           }
           resolve(result);
+        }
+      }
+    );
+  });
+};
+
+// 특정 책장의 모든 책 개수
+exports.getTotalBooks = function (bookshelf_id) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT count(item_id) AS total_book FROM iBookShare.Bookshelf_item WHERE bookshelf_id = ?`,
+      [bookshelf_id],
+      function (err, res) {
+        if (err) {
+          reject(err);
+        } else {
+          var result = JSON.parse(JSON.stringify(res[0]));
+          resolve(result.total_book);
+        }
+      }
+    );
+  });
+};
+
+exports.countBooksCategory = function (bookshelf_id) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT category AS category_id, count(category) AS total FROM Bookshelf_item WHERE Bookshelf_id = ? GROUP BY category`,
+      [bookshelf_id],
+      function (err, res) {
+        if (err) {
+          reject(err);
+        } else {
+          if (res.length != 0) {
+            var data;
+            var result = new Array();
+            for (let i = 0; i < res.length; i++) {
+              data = JSON.parse(JSON.stringify(res[i]));
+              result.push(data);
+              if (i == res.length - 1) {
+                resolve(result);
+              }
+            }
+          } else {
+            resolve(0);
+          }
+        }
+      }
+    );
+  });
+};
+
+exports.countBooksDate = function (bookshlef_id) {
+  var result = {};
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT count(item_id) AS total FROM Bookshelf_item WHERE bookshelf_id = ? AND created BETWEEN DATE_ADD(NOW(),INTERVAL -1 DAY ) AND NOW()`,
+      [bookshlef_id],
+      function (err, res) {
+        if (err) reject(err);
+        else {
+          var oneData = JSON.parse(JSON.stringify(res[0]));
+          var today_total = oneData.total;
+          result.today_read = today_total;
+          db.query(
+            `SELECT count(item_id) AS total FROM Bookshelf_item WHERE bookshelf_id = ? AND created BETWEEN DATE_ADD(NOW(),INTERVAL -1 WEEK ) AND NOW()`,
+            [bookshlef_id],
+            function (err, res) {
+              if (err) reject(err);
+              else {
+                oneData = JSON.parse(JSON.stringify(res[0]));
+                var week1_total = oneData.total;
+                result.week1_read = week1_total;
+                db.query(
+                  `SELECT count(item_id) AS total FROM Bookshelf_item WHERE bookshelf_id = ? AND created BETWEEN DATE_ADD(NOW(),INTERVAL -2 WEEK ) AND NOW()`,
+                  [bookshlef_id],
+                  function (err, res) {
+                    if (err) reject(err);
+                    else {
+                      oneData = JSON.parse(JSON.stringify(res[0]));
+                      var week2_total = oneData.total - week1_total;
+                      result.week2_read = week2_total;
+                      result.minus = week1_total - week2_total;
+                      resolve(result);
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
+      }
+    );
+  });
+};
+
+exports.getBookshelfId_sameKids = function (memId, age) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT * FROM Kids AS kids JOIN Bookshelf AS shelf ON kids.mem_id = shelf.mem_id WHERE kids.mem_id != ? AND kids.age = ?`,
+      [memId, age],
+      function (err, res) {
+        if (err) reject(err);
+        else {
+          if (res.length != 0) {
+            var data, bookshelf_id;
+            var result = new Array();
+            for (let i = 0; i < res.length; i++) {
+              data = JSON.parse(JSON.stringify(res[i]));
+              bookshelf_id = data.bookshelf_id;
+              result.push(bookshelf_id);
+              if (i == res.length - 1) {
+                resolve(result);
+              }
+            }
+          } else {
+            resolve(0);
+          }
         }
       }
     );
